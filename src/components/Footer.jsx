c56b3de4ecb4c210789f9eff1455028f5ff1b86d
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-11-03 21:16:40 -0700

    eddit crafted statement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 import "./Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+library.add(faGithub, faLinkedin);
 
 function Footer({storeInfo}) {
 
@@ -14,7 +18,11 @@ function Footer({storeInfo}) {
         {storeInfo.tel}
       </span>
       <span className="copywrite">
-        ©2023 Mr.Greek Donair | All rights Reserved.
+        {/* ©2023 Mr.Greek Donair | All rights Reserved. */}
+        Handcrafted by Farzaneh &ensp;
+        <a href="https://github.com/FarzanehSa/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon="fa-brands fa-github" className='h-icon'/>
+        </a>,  ©2023
       </span>
     </div>
   );
