8254f09005f8f83c7184ddd724a620387583b560
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-11-13 22:19:06 -0800

    take mui color var to seprate file

---
+++ src/components/dashboard/variable.js
@@ -0,0 +1,6 @@
+// const admin_color = 'LightSeaGreen';
+// const admin_color_sec = 'PaleTurquoise';
+// const admin_color = 'SaddleBrown';
+const admin_color = '#8B4513';
+const admin_color_sec = '#f0bc96';
+export {admin_color, admin_color_sec};
